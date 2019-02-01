3805cb1f6a205a044fe1dc38ceb06dc38c92542a
Nahomy Puente <nahomypuentec@gmail.com>
2019-02-01 18:57:40 -0300

    Modified class Main

--- app/assets/containers/main.jsx
+++ app/assets/containers/main.jsx
@@ -2,39 +2,105 @@ import React from 'react';
 import axios from 'axios';
 import Home from '../presentational/home';
 
-
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       loading: true,
-      locations: []
+      locations: [],
+      isLoggedIn: false,
+      isProvider: false,
+      redirect: false,
+      /*ID de algun proveedor solicitado en redirect*/
+      id: 0
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleSubmitLogout = this.handleSubmitLogout.bind(this);
   }
 
   componentDidMount() {
     axios
       .get("/api/locations")
       .then(
-        response => this.setState({locations: response.data, loading: false})
-      ).catch(
+        response => {
+          console.log(response)
+          console.log(localStorage.getItem("isLoggedIn"))
+          if (localStorage.getItem("isLoggedIn") == 'true') {
+            this.setState({ isLoggedIn: true });
+          }
+          if (localStorage.getItem("isProvider") == 'true') {
+            this.setState({ isProvider: true });
+          }
+          console.log(this.state.isLoggedIn)
+          this.setState({locations: response.data, loading: false});
+      }).catch(
         error => {
+          console.log(error)
           if (!error.response)
             alert(error);
           else if (error.response.data && error.response.status !== 404)
-            alert(error.response.data);
+            alert(error.response.data)
           else
             alert(error.response.statusText);
-          this.setState({loading: false});
+          this.setState({ loading: false });
         }
       );
+  }
+
+  handleClick(id) {
+    console.log(id)
+    this.setState ({
+      id: parseInt(id)
+    });
+  }
+
+  handleSubmit(evt) {
+    console.log(this.state.id)
+    this.setState({ loading: true }, () => {
+      this.setState({ redirect: true, loading: false });
+    });
+  }
+
+  handleSubmitLogout() {
+    this.setState({ loading: true }, () => {
+      axios
+        .post('/api/logout',
+          this.state.logout)
+        .then(
+          response => {
+            console.log(response)
+            if ((localStorage.getItem("isLoggedIn")) == "true"){
+              localStorage.clear();
+              this.setState({ isLoggedIn: false });
+            }
+          }
+        ).catch(
+          error => {
+            console.log(error);
+            if (!error.response)
+              alert(error);
+            else if (error.response.data && error.response.status !== 404)
+              alert(error.response.data)
+            else
+              alert(error.response.statusText);
+        }).finally(()=>{
+          this.setState({ loading: false });
+        });
+    });
   }
 
   render() {
     return (
       <Home loading={this.state.loading}
-            locations={this.state.locations}/>
+            locations={this.state.locations}
+            handleSubmit={this.handleSubmit}
+            handleSubmitLogout={this.handleSubmitLogout}
+            handleClick={this.handleClick}
+            redirect={this.state.redirect}
+            isLoggedIn={this.state.isLoggedIn}
+            isProvider={this.state.isProvider}
+            id={this.state.id}/>
     );
   }
 }
--- app/assets/presentational/home.jsx
+++ app/assets/presentational/home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, Nav, NavItem, NavLink, Navbar,
+         NavbarBrand, Button } from 'reactstrap';
 
 export default function Home(props) {
-  let { loading, locations } = props;
+  let { loading, locations, handleSubmit, id, isLoggedIn, redirect,
+        handleClick, handleSubmitLogout, isProvider } = props;
 
   if (loading) {
     return (
@@ -15,18 +19,56 @@ export default function Home(props) {
     );
   } else {
     return (
-      <div className="body">
-        <div className="main-div">
-          <h1 className="display-3 text-center mb-5">
-            ¿Qu&eacute; vas a comer hoy?</h1>
-          <ul>
-            { locations.map((loc, idx) => (
-                <li key={idx}
-                    className={loc.id.toString()}>{ loc.name }</li>
-              ))
-            }
-          </ul>
-        </div>
+      <div>
+        { isLoggedIn ? (
+          <Navbar color="primary" light expand="sm">
+            <NavbarBrand href="/">Deliveru</NavbarBrand>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/profile">Perfil</NavLink>
+                </NavItem>
+                <NavItem>
+                  <Form onSubmit={handleSubmitLogout}>
+                    <Button>Cerrar Sesión</Button>
+                  </Form>
+                </NavItem>
+              </Nav>
+          </Navbar>
+          ) : (
+          <Navbar color="primary" light expand="sm">
+            <NavbarBrand href="/">Deliveru</NavbarBrand>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/signup">Registrate</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/login">Iniciar Sesión</NavLink>
+                </NavItem>
+              </Nav>
+          </Navbar>
+          )
+        }
+        <h1 className="display-3 text-center mb-5">
+          ¿Qué vas a comer hoy?</h1>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="Select">Elegí tu ubicacion</Label>
+            <Input type="select" name="location" id="Select">
+              { locations.map((loc, idx) => (
+                <option key={idx} className={loc.id.toString()}
+                  onClick={() => { handleClick(loc.id) }}>{ loc.name }</option>
+                ))
+              }
+            </Input>
+          </FormGroup>
+          <Button>Buscar</Button>
+        </Form>
+        { (isLoggedIn) ? (
+            (redirect ? (<Redirect to={"/delivery/" + id}/>) : (""))
+          ) : (
+            (redirect ? (<Redirect to={"/login"}/>) : (""))
+          )
+        }
       </div>
     );
   }
@@ -34,5 +76,9 @@ export default function Home(props) {
 
 Home.propTypes = {
   loading: PropTypes.bool.isRequired,
-  locations: PropTypes.array.isRequired
+  locations: PropTypes.array.isRequired,
+  id: PropTypes.number.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+  isProvider: PropTypes.bool.isRequired,
+  redirect: PropTypes.bool.isRequired
 };
